536f10ccc00a828965aa547fd094bcf2e83342aa
Kohega <45211407+Kohega@users.noreply.github.com>
2025-08-29 00:11:38 +0200

    chore

--- build_files/build.sh
+++ build_files/build.sh
@@ -31,8 +31,6 @@ echo_group /ctx/install_packages.sh
 
 # Install RPMs
 log "Installing apps from RPM"
-cd /ctx/rpm
-wget "https://litter.catbox.moe/2wd36xqfpvdpfvq8.rpm"
 for rpm_file in ctx/rpm/*.rpm; do
     if [ -f "$rpm_file" ]; then
         dnf5 install -y "$rpm_file"
